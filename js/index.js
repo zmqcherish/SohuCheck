38e7cc99b16eaf2741d92c27732af1a746d9bb09
cherish <cherishzhang@sohu-inc.com>
2021-03-06 16:58:07 +0800

    0306

--- js/index.js
+++ js/index.js
@@ -11,9 +11,10 @@ function mainfun(){
         setTimeout("mainfun()",2000);
       }else{
           var trs = Array.prototype.slice.call(children);
+		  handle(trs[0]);
           trs.forEach(function(tr){
             try {
-                handle(tr)
+                // handle(tr)
             } catch (error) {
                 
             }  
@@ -26,10 +27,95 @@ function mainfun(){
 function handle(tr){
     var td = tr.children[0].children[0];
     var txt = td.innerText;
-    if(txt.indexOf("无打卡")!=-1){
-        // console.log(1);
-    }
-    else if(txt.indexOf("正常")!=-1){
+    if(txt.indexOf("无打卡")!=-1 && txt.indexOf("异常")!=-1){
+        console.log(1);
+		var trrs = td.children[0].children;
+        //var times = trrs[3].innerText;
+        //var end = times.split("~")[1].split(":");
+        trrs[8].firstChild.firstChild.click();
+		//console.log(td);
+        var content = td.children[1].firstChild.firstChild.children;
+        var contentTable = content[0].firstChild.children[2];
+		console.log(contentTable);
+        if(contentTable.childElementCount!=2){
+            var cTable = contentTable.firstChild.children;
+           // var endHour = Number(end[0]);
+            //var endMin = Number(end[1]);
+			cTable[0].firstChild.value  = "fa68ab6c-78c7-4270-97ca-76f76550b2db";
+			addOnChange(cTable[0].firstChild);
+             
+		}
+		content[0].children[1].firstChild.click();
+	}
+	else if(txt.indexOf("异常")!=-1){
+		debugger
+		var trrs = td.children[0].children;
+        var times = trrs[3].innerText;
+        var end = times.split("~")[1].split(":");
+		var start = times.split("~")[0].split(":");
+        trrs[8].firstChild.firstChild.click();
+		var content = td.children[1].firstChild.firstChild.children;
+        var contentTable = content[0].firstChild.children[2];
+        //if(contentTable.childElementCount!=2){
+		var cTable = contentTable.firstChild.children;
+		var endHour = Number(end[0]);
+		var endMin = Number(end[1]);
+		var startHour = Number(start[0]);
+		var startMin = Number(start[1]);
+		cTable[2].children[1].value = 9;
+		addOnChange(cTable[2].children[1]);
+		cTable[2].children[2].value = startMin;
+		addOnChange(cTable[2].children[2]);
+		//debugger
+		cTable[5].firstChild.children[0].click();
+		if(start[0][0]=='0'){
+			//cTable[3].children[1].value = start[0][1];
+		}else{
+			//cTable[3].children[1].value = start[0];
+		}
+		//cTable[3].children[1].value = start[0];
+		//addOnChange(cTable[3].children[1]);
+		if(start[1][0]=='0'){
+			//cTable[3].children[2].value = start[1][1];
+		}else{
+			//cTable[3].children[2].value = start[1];
+		}
+		//cTable[3].children[2].value = start[1];
+		//addOnChange(cTable[3].children[2]);
+		//}
+		cTable[5].firstChild.children[1].click();
+		debugger
+		var cTable2 = contentTable.children[1].children;
+		var val;
+		if(endHour>21||(endHour==21&&endMin>=30)){
+			val = "e31d3265-5b6c-40d7-ba6e-07a8cd3ada74";//日常加班 津贴
+		}
+		else{
+			val = "b3cea43e-0df6-4730-8d94-49acbdcb45be";//日常加班 报销
+		}
+		
+		cTable2[0].firstChild.value = val;
+		addOnChange(cTable2[0].firstChild);
+
+		cTable2[2].children[1].value = 18;
+		addOnChange(cTable2[2].children[1]);
+		
+		cTable2[2].children[2].value = 30;
+		addOnChange(cTable2[2].children[2]);
+		
+		cTable2[3].children[1].value = end[0];
+		addOnChange(cTable2[3].children[1]);
+
+		if(end[1][0]=='0'){
+			cTable2[3].children[2].value = end[1][1];
+		}else{
+			cTable2[3].children[2].value = end[1];
+		}
+				
+		document.getElementsByClassName('btnAiCon')[0].children[0].click();
+		content[0].children[1].firstChild.click();
+	}
+    else if(txt.indexOf("正常")!=-1) {
         var trrs = td.children[0].children;
         var times = trrs[3].innerText;
         var end = times.split("~")[1].split(":");
@@ -42,7 +128,7 @@ function handle(tr){
             var endHour = Number(end[0]);
             var endMin = Number(end[1]);
 
-            if(txt.indexOf("休息日")!=-1){
+            if(txt.indexOf("休息日")!=-1) {
                 var start = times.split("~")[0].split(":");
                 
                 cTable[0].firstChild.value  = "62728b0b-538b-423b-ba2b-c5928691b2db";//周末加班 津贴
